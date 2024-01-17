6358ae3c7adf70e27ea370c96a9788508993fdc4
Stanley Metray <stanleymetray@gmail.com>
2024-01-17 14:04:20 +0530

    Edit Functionality

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ class user_details {
     }
 }
 
-function handleFormSubmit(event){
+function handleFormSubmit(event) {
     event.preventDefault();
-    
+
     const username = document.getElementById("username").value;
     const email = document.getElementById("email").value;
     const phone = document.getElementById("phone").value;
@@ -19,11 +19,45 @@ function handleFormSubmit(event){
 
     const list = document.getElementById("list");
     list.innerHTML = "";
-    users.forEach((value)=>{
+    users.forEach((value) => {
         const li = document.createElement("li");
-        li.innerText = value.username+" - "+value.email+" - "+value.phone;
+        li.innerHTML = value.username + " - " + value.email + " - " + value.phone + "<button class='btn-delete'>X</button><button class='btn-edit'>Edit</button>";
         list.appendChild(li);
     });
 
     localStorage.setItem("User Details", JSON.stringify(users));
+
+    const btnDelete = document.getElementsByClassName("btn-delete");
+    Array.from(btnDelete).forEach((element) => {
+        element.addEventListener("click", (e) => {
+            e.target.parentElement.remove();
+        });
+    });
+
+    // Edit Button Functionality
+    const btnEdit = document.getElementsByClassName("btn-edit");
+    
+    Array.from(btnEdit).forEach((element) => {
+
+        element.addEventListener("click", (e) => {
+            let name = e.target.parentElement.innerText.split(" ")[0];
+            e.target.parentElement.remove();
+
+            let users = JSON.parse(localStorage.getItem("User Details"));
+            let index;
+            for (index = 0; index < users.length; index++) {
+                if (name == users[index].username) {
+                    document.getElementById("username").value=users[index].username;
+                    document.getElementById("email").value=users[index].email;
+                    document.getElementById("phone").value=users[index].phone;
+                    break;
+                }
+            }
+
+            users.splice(index, 1);
+            localStorage.removeItem("User Details");
+            localStorage.setItem("User Details", JSON.stringify(users));
+        });
+
+    });
 }
