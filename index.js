6de51920039e902cb0f81b735e4788983926e3d9
Stanley Metray <stanleymetray@gmail.com>
2024-01-26 00:30:16 +0530

    Too many promises in life task

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
-function fun1(){
+const user = {
+    lastActivityTime: null,
+    posts: []
+};
 
-    console.log('a')
-    
-    return () => {
-    
-    console.log('b')
-    
-    }}
-    
-    
-    
-    fun1()
+function createPost(post) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            user.posts.push(post);
+            resolve(post);
+        }, 1000);
+    });
+}
+
+function updateLastUserActivityTime() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            user.lastActivityTime = new Date();
+            resolve(user.lastActivityTime);
+        }, 1000);
+    });
+}
+
+function deletePost() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (user.posts.length > 0) {
+                const deletedPost = user.posts.pop();
+                resolve(deletedPost);
+            } else {
+                reject("No posts to delete");
+            }
+        }, 1000);
+    });
+}
+
+
+createPost("First post")
+    .then((post) => updateLastUserActivityTime().then((lastActivityTime) => ({ post, lastActivityTime })))
+    .then(({ post, lastActivityTime }) => {
+        console.log("User's posts:", user.posts);
+        console.log("User's last activity time:", lastActivityTime);
+        return deletePost();
+    })
+    .then((deletedPost) => {
+        console.log("Deleted post:", deletedPost);
+        console.log("Remaining posts:", user.posts);
+    })
+    .catch((error) => {
+        console.error("Error:", error);
+    });
